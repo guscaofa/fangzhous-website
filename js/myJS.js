72890a3e2e170521c695972efab35de04d8d9543
caofa <vincet918@gmail.com>
2015-10-21 20:18:14 +0200

    first update

---
+++ js/myJS.js
@@ -0,0 +1,18 @@
+
+$(function(){
+    $("#page2").hide();
+
+});
+
+$("#pagenumber2").click(function(){
+    $("#page2").show();
+    $("#page1").hide();
+
+});
+
+
+$("#pagenumber1").click(function(){
+    $("#page1").show();
+    $("#page2").hide();
+
+});
